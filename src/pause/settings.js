abfdd0a22a7a46797bba62e6c6f7fe5e55de3f47
raz0red <raz0red@gmail.com>
2025-09-21 10:14:44 -0700

    shade

--- src/pause/settings.js
+++ src/pause/settings.js
@@ -15,6 +15,8 @@ import {
   // ScreenControlsSelect,
   Switch,
   WebrcadeContext,
+  BlurImage,
+  ShaderSettingsTab,
 } from '@webrcade/app-common';
 import { VkTransparencySelect } from './vktransparencyselect';
 import { DiskSelect } from './diskselect';
@@ -32,22 +34,27 @@ export class CommodoreSettingsEditor extends Component {
   componentDidMount() {
     const { emulator } = this.props;
 
+    const values = {
+      swapControllers: emulator.getSwapControllers(),
+      origBilinearMode: emulator.getPrefs().isBilinearEnabled(),
+      bilinearMode: emulator.getPrefs().isBilinearEnabled(),
+      origScreenSize: emulator.getPrefs().getScreenSize(),
+      screenSize: emulator.getPrefs().getScreenSize(),
+      origScreenControls: emulator.getPrefs().getScreenControls(),
+      screenControls: emulator.getPrefs().getScreenControls(),
+      origVkTransparency: emulator.getPrefs().getVkTransparency(),
+      vkTransparency: emulator.getPrefs().getVkTransparency(),
+      origMediaIndex: emulator.getMediaIndex(),
+      mediaIndex: emulator.getMediaIndex()
+      // origVkCloseOnEnter: emulator.getPrefs().getVkCloseOnEnter(),
+      // vkCloseOnEnter: emulator.getPrefs().getVkCloseOnEnter(),
+    };
+
+    this.shaderService = this.props.emulator.getShadersService();
+    this.shaderService.addEditorValues(values);
+
     this.setState({
-      values: {
-        swapControllers: emulator.getSwapControllers(),
-        origBilinearMode: emulator.getPrefs().isBilinearEnabled(),
-        bilinearMode: emulator.getPrefs().isBilinearEnabled(),
-        origScreenSize: emulator.getPrefs().getScreenSize(),
-        screenSize: emulator.getPrefs().getScreenSize(),
-        origScreenControls: emulator.getPrefs().getScreenControls(),
-        screenControls: emulator.getPrefs().getScreenControls(),
-        origVkTransparency: emulator.getPrefs().getVkTransparency(),
-        vkTransparency: emulator.getPrefs().getVkTransparency(),
-        origMediaIndex: emulator.getMediaIndex(),
-        mediaIndex: emulator.getMediaIndex()
-        // origVkCloseOnEnter: emulator.getPrefs().getVkCloseOnEnter(),
-        // vkCloseOnEnter: emulator.getPrefs().getVkCloseOnEnter(),
-      },
+      values: values,
     });
   }
 
@@ -66,7 +73,7 @@ export class CommodoreSettingsEditor extends Component {
     return (
       <EditorScreen
         showCancel={true}
-        onOk={() => {
+        onOk={async () => {
           let change = false;
           emulator.setSwapControllers(values.swapControllers);
           if (values.origBilinearMode !== values.bilinearMode) {
@@ -102,6 +109,10 @@ export class CommodoreSettingsEditor extends Component {
           if (change) {
             emulator.getPrefs().save();
           }
+
+          // Set the shader
+          await this.shaderService.setShader(values.shaderId);
+
           onClose();
         }}
         onClose={onClose}
@@ -137,12 +148,26 @@ export class CommodoreSettingsEditor extends Component {
             ),
           },
           {
+            image: BlurImage,
+            label: 'Shader Settings',
+            content: (
+              <ShaderSettingsTab
+                shaderService={this.shaderService}
+                emulator={emulator}
+                isActive={tabIndex === 2}
+                setFocusGridComps={setFocusGridComps}
+                values={values}
+                setValues={setValues}
+              />
+            )
+          },
+          {
             image: KeyboardWhiteImage,
             label: 'Virtual Keyboard Settings',
             content: (
               <CommodoreVirtualKeyboardTab
                   emulator={emulator}
-                  isActive={tabIndex === 2}
+                  isActive={tabIndex === 3}
                   setFocusGridComps={setFocusGridComps}
                   values={values}
                   setValues={setValues}
